37984a915c3cfa739d46368f57690daf938bbde9
Tomas Birbe <tomas.birbe@gmail.com>
2023-05-06 12:25:34 -0300

    (main): add mising plugins to eslint and some default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <h1>Hello, World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        voluptatem beatae adipisci laborum fugiat nulla, nisi sint in quaerat
+        atque, debitis ea temporibus aliquam est nesciunt voluptates impedit,
+        illum unde!
+      </p>
+    </main>
+  );
 }
 
 export default App;
