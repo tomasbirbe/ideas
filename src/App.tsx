89c36d52242177d1c33f97453db72efb34d4a762
Tomas Birbe <tomas.birbe@gmail.com>
2023-05-08 13:36:29 -0300

    (main): test how to handle x and y coords of the pointer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Main = styled.main({
+  height: "100%",
+  width: "100%",
+});
+
+const SMenu = styled.div<{ x: string; y: string }>(({ x, y }) => ({
+  transform: `translate(${Number(x) - 50}px, ${Number(y) - 50}px)`,
+  background: "#1c1c1c",
+  borderRadius: "4px",
+  width: "100px",
+  height: "100px",
+}));
+
+const Menu = ({ x, y }: { x: string; y: string }) => {
+  return <SMenu x={x} y={y}></SMenu>;
+};
+
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  function toggleMenu(event: any) {
+    // setShowMenu(!showMenu);
+
+    setCoords({ x: event.pageX, y: event.pageY });
+  }
+
   return (
-    <main>
-      <h1>Hello, World!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        voluptatem beatae adipisci laborum fugiat nulla, nisi sint in quaerat
-        atque, debitis ea temporibus aliquam est nesciunt voluptates impedit,
-        illum unde!
-      </p>
-    </main>
+    <Main id="main" onClick={toggleMenu}>
+      {showMenu && <Menu x={coords.x.toString()} y={coords.y.toString()} />}
+    </Main>
   );
 }
 
